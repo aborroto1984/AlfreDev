c207bb49dad13b0695fbf638e6d81fe15ff2faae
Alfredo Borroto <alborroto@student.fullsail.edu>
2023-09-13 17:37:57 -0400

    Changed the header animation

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,23 +1,27 @@
 import {AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 import { timer } from 'rxjs';
 import { trigger, state, style, animate, transition } from '@angular/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
   animations: [
-    trigger('fade', [
-      state('show', style({ opacity: 1 })),
-      state('hide', style({ opacity: 0 })),
-      transition('hide => show', animate('500ms ease-in')),
-      transition('show => hide', animate('500ms ease-out')),
-    ]),],
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(20px)' }),
+        animate('500ms', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+    ]),
+  ],
 })
 export class MainComponent implements OnInit{
+  // @ts-ignore
+  @ViewChild('h1') allTitles: ElementRef;
   isStatic = false
-  constructor() {}
-
+  elementVisible = false;
+  constructor(private el: ElementRef) {}
   ngOnInit() {
     // Create a timer that emits a value after 10 seconds
     const timer$ = timer(10000); // 10000 milliseconds = 10 seconds
@@ -27,5 +31,22 @@ export class MainComponent implements OnInit{
       // This code will be executed after 10 seconds
       this.isStatic = true;
     });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (!this.elementVisible) {
+      const element = this.el.nativeElement.querySelector('test');
+      const rect = element.getBoundingClientRect();
+
+      if (rect.top <= window.innerHeight - 100) {
+        this.elementVisible = true;
+        // Apply the animation class when the element is visible
+        element.classList.add('animate-fade-in');
+      }
+      else{
+        this.elementVisible = false;
+      }
+    }
   }
 }
