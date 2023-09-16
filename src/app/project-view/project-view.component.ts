9ce110d9e4d2f196b87d227639cab5586d18a456
Alfredo Borroto <alborroto@student.fullsail.edu>
2023-09-16 17:07:40 -0400

    Created the projects element

---
+++ src/app/project-view/project-view.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'project-view',
+  templateUrl: './project-view.component.html',
+  styleUrls: ['./project-view.component.scss']
+})
+export class ProjectViewComponent {
+name = 'Perfect Cut Calculator';
+imagePath = 'assets/PerfectCut.png';
+videoPath = '';
+codePath = '';
+exePath = ''
+
+}
