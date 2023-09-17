ee406a7c9b42cc6904f756c217543f1e98615f18
Alfredo Borroto <aborroto1984@hotmail.com>
2023-09-17 11:51:08 -0400

    Finished the alignment of component of the project card

--- src/app/project-view/project-view.component.ts
+++ src/app/project-view/project-view.component.ts
@@ -6,8 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-view.component.scss']
 })
 export class ProjectViewComponent {
-name = 'Perfect Cut Calculator';
-imagePath = '';
+name = 'Black Jack';
+description ='Black Jack was a project developed in Visual Studios using C#. It is a console application where you can play the blackjack game against the computer using a simple betting system where you start the game with $2000 and place bets before the game starts. While writing this project, I used Object Oriented Programming to write the Hand, Card, Deck, and Game classes. The factory design pattern is also applied to construct the card objects.';
+imagePath = 'assets/BlackJack-topaz.png';
 videoPath = '';
 codePath = '';
 exePath = ''
